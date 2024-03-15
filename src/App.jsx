2880f158893d667276bd39adc91b0fed27e98f00
Luis David <cusiluis04@gmail.com>
2024-03-14 22:21:13 -0500

    Renderizado de imagen

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import { useEffect, useState } from "react"
 const FACT_RAMDON = `https://catfact.ninja/fact`
 // const API_ENDPOINT = `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
 export function App (){
-    const [fact, setFact] = useState('lorem ipsum cat')
+    const [fact, setFact] = useState()
+    const [imageUrl, setImageUrl] = useState()
 
     // Utilizamos useEffect para hacer el fetching de datos
     useEffect(() =>{
@@ -13,13 +14,26 @@ export function App (){
         // Obtenemos la respuesta y la pasamos a formato JSON
             .then(response => response.json())
         // Accedemos al hecho
-            .then(data =>setFact(data.fact))
+            .then(data => {
+                const { fact } = data
+                setFact(fact)
+
+                const firstWord = fact.split(' ', 3).join(' ')                
+
+                fetch(`https://cataas.com/cat/gif/says/${firstWord}?filter=mono&fontColor=orange&fontSize=20&type=square`)
+                    .then(response => {                        
+                        const { url } = response
+                        setImageUrl(url)
+                    })     
+            })
     }, [])
 
     return(
         <main>
             <h1>App de gatos xd</h1>
-            <p>{fact}</p>
+            {fact && <p>{fact}</p>}
+            {imageUrl && <img src={`${imageUrl}`} alt={`Image extracted using the
+            first rhee words for ${fact}`} />}
         </main>
         
     )
