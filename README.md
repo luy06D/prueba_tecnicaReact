240b143617fdf88f27922cc0d65b87c0a6902134
luy06D <cusiluis04@gmail.com>
2024-06-01 21:37:19 -0500

    Prueba tecnica completa

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 APIs:
 - Facts Random: https://catfact.ninja/fact
 - Image random: https://cataas.com/
-- endpoint : `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
+- endpoint : `https://cataas.com/cat/says/xddddd?fontSize=50&fontColor=red`
 
 Recupera un hecho aleatorio de gatos de la primera API y muestra una imagen de un gato con la primera palabra del hecho recuperado usando la segunda API.
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
 import { useEffect, useState } from "react"
+import '../src/appStyle.css'
 
 // Constante que almacena el api endpoint
 const FACT_RAMDON = `https://catfact.ninja/fact`
-// const API_ENDPOINT = `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
+// const API_ENDPOINT = `https://cataas.com/cat/says/${text_fact}?fontSize=50&fontColor=red`
 export function App (){
-    const [fact, setFact] = useState('lorem ipsum cat')
+    const [fact, setFact] = useState()
+    const [imgUrl, setImgUrl] = useState()
 
     // Utilizamos useEffect para hacer el fetching de datos
     useEffect(() =>{
-        //Realizamos una petocion GET - la url nos da un JSON
+        //Realizamos una peticion GET - la url nos da un JSON
         fetch(FACT_RAMDON)
         // Obtenemos la respuesta y la pasamos a formato JSON
             .then(response => response.json())
         // Accedemos al hecho
-            .then(data =>setFact(data.fact))
+            .then(data =>{
+                const {fact} = data
+                setFact(fact)
+                const text_fact = fact.split(' ', 1)
+                
+                fetch(`https://cataas.com/cat/says/${text_fact}?fontSize=50&fontColor=red`)
+                .then(response => response)
+                .then(data =>{
+                    const {url} = data
+                    setImgUrl(url)
+                })
+            })
+                 
     }, [])
 
     return(
         <main>
             <h1>App de gatos xd</h1>
             <p>{fact}</p>
+            <img src={imgUrl} alt="" />
         </main>
         
     )
